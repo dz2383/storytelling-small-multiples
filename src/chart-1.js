cf2659d68e657b064cdbe6c680e820a6a9ab251a
Dian <dz2382@columbia.edu>
2018-09-29 21:55:59 -0400

    first chart

---
+++ src/chart-1.js
@@ -0,0 +1,185 @@
+import * as d3 from 'd3'
+
+// Set up margin/height/width
+var margin = { top: 60, left: 50, right: 200, bottom: 30 }
+
+var height = 600 - margin.top - margin.bottom
+var width = 600 - margin.left - margin.right
+
+// Add your svg
+var svg = d3
+  .select('#chart-1')
+  .append('svg')
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`)
+
+// Create a time parser (see hints)
+let parseTime = d3.timeParse('%B-%y')
+
+// Create your scales
+var xPositionScale = d3.scaleLinear().range([0, width])
+var yPositionScale = d3
+  .scaleLinear()
+  .domain([180, 340])
+  .range([height, 0])
+var colorScale = d3
+  .scaleOrdinal()
+  .domain([
+    'Mountain',
+    'Pacific',
+    'West South Central',
+    'South Atlantic',
+    'U.S.',
+    'West North Central',
+    'New England',
+    'East South Central',
+    'Middle Atlantic',
+    'East North Central'
+  ])
+  .range([
+    '#d53e4f',
+    '#f46d43',
+    '#fdae61',
+    '#fee08b',
+    '#ffffbf',
+    '#e6f598',
+    '#abdda4',
+    '#66c2a5',
+    '#3288bd',
+    '#92c5de'
+  ])
+
+// Create a d3.line function that uses your scales
+// line generator
+var line = d3
+  .line()
+  .x(function(d) {
+    return xPositionScale(d.datetime)
+  })
+  .y(function(d) {
+    return yPositionScale(d.price)
+  })
+  .curve(d3.curveMonotoneX)
+
+// Read in your housing price data
+d3.csv(require('./housing-prices.csv'))
+  .then(ready)
+  .catch(err => {
+    console.log('Failed with', err)
+  })
+
+// Write your ready function
+
+function ready(datapoints) {
+  // Convert your months to dates
+  datapoints.forEach(d => {
+    d.datetime = parseTime(d.month)
+  })
+  let dates = datapoints.map(function(d) {
+    return d.datetime
+  })
+  console.log(datapoints)
+
+  xPositionScale.domain(d3.extent(dates))
+
+  // Get a list of dates and a list of prices
+
+  // Group your data together
+  var nested = d3
+    .nest()
+    .key(d => d.region)
+    .entries(datapoints)
+  console.log(nested)
+
+  // Draw your lines
+
+  svg
+    .selectAll('.price-category')
+    .data(nested)
+    .enter()
+    .append('path')
+    .attr('class', 'price-category')
+    .attr('d', function(d) {
+      return line(d.values)
+    })
+    .attr('stroke', d => colorScale(d.key))
+    .attr('stroke-width', 2)
+    .attr('fill', 'none')
+    .lower()
+
+  svg
+    .selectAll('.last-month')
+    .data(nested)
+    .enter()
+    .append('circle')
+    .attr('class', 'last-month')
+    .attr('cy', d => yPositionScale(d.values[0].price))
+    .attr('cx', d => xPositionScale(d.values[0].datetime))
+    // This also works because all circles have same x position
+    // .attr('cx', width)
+    .attr('fill', d => colorScale(d.key))
+    .attr('r', 3)
+
+  // Add your text on the right-hand side
+  svg
+    .selectAll('.region-category')
+    .data(nested)
+    .enter()
+    .append('text')
+    .attr('class', 'region-category')
+    .attr('y', d => yPositionScale(d.values[0].price))
+    .attr('x', width + 7)
+    .attr('dy', -5)
+    .attr('alignment-baseline', 'hanging')
+    .attr('font-size', 12)
+    // .attr('text-anchor', 'start')
+    .text(d => d.key)
+
+  // Add your title
+  svg
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', 0 - margin.top / 2)
+    .attr('text-anchor', 'middle')
+    // Style is same as attr here
+    // But use Style in <div>
+    .style('font-size', 18)
+    // .style('text-decoration', 'underline')
+    .text('U.S. Housing Prices Fall in Winter')
+
+  // Add the shaded rectangle
+  var december16 = parseTime('December-16')
+  var february17 = parseTime('February-17')
+  // var maxPrice = d3.max(datapoints, d => d.price)
+  svg
+    .append('rect')
+    .attr('x', xPositionScale(december16))
+    // .attr('y', yPositionScale(340))
+    .attr('y', yPositionScale(340))
+    .attr('width', xPositionScale(february17) - xPositionScale(december16))
+    .attr('height', height)
+    .attr('fill', 'lightgray')
+    .attr('opacity', 0.5)
+
+  // Add your axes
+  var xAxis = d3
+    .axisBottom(xPositionScale)
+    .tickFormat(d3.timeFormat('%b %y'))
+    .ticks(10)
+  svg
+    .append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+
+  var yAxis = d3
+    .axisLeft(yPositionScale)
+    .tickValues([200, 220, 240, 260, 280, 300, 320])
+
+  svg
+    .append('g')
+    .attr('class', 'axis y-axis')
+    .call(yAxis)
+}
---
+++ src/chart-2.js
@@ -0,0 +1,132 @@
+import * as d3 from 'd3'
+
+// Set up margin/height/width
+var margin = { top: 30, left: 20, right: 20, bottom: 20 }
+
+var height = 150 - margin.top - margin.bottom
+var width = 100 - margin.left - margin.right
+
+// I'll give you the container
+var container = d3.select('#chart-2')
+
+// Create your scales
+var xPositionScale = d3.scaleLinear().range([0, width])
+var yPositionScale = d3
+  .scaleLinear()
+  .domain([0, 0.3])
+  .range([height, 0])
+
+// var colorScale = d3
+//   .scaleOrdinal()
+//   .domain(['ASFR_jp', 'ASFR_us'])
+//   .range(['#e34a33', '#2b8cbe'])
+
+// Create a d3.line function that uses your scales
+var lineus = d3
+  .line()
+  .x(function(d) {
+    return xPositionScale(d.Age)
+  })
+  .y(function(d) {
+    return yPositionScale(d.ASFR_us)
+  })
+  .curve(d3.curveMonotoneX)
+
+var linejp = d3
+  .line()
+  .x(function(d) {
+    return xPositionScale(d.Age)
+  })
+  .y(function(d) {
+    return yPositionScale(d.ASFR_jp)
+  })
+  .curve(d3.curveMonotoneX)
+
+// Read in your data
+d3.csv(require('./fertility.csv'))
+  .then(ready)
+  .catch(err => {
+    console.log('Failed with', err)
+  })
+
+// Build your ready function that draws lines, axes, etc
+function ready(datapoints) {
+  // Group your data together
+  var nested = d3
+    .nest()
+    .key(d => d.Year)
+    .entries(datapoints)
+  console.log(nested)
+
+  // Draw your lines
+  container
+    .selectAll('svg')
+    .data(nested)
+    .enter()
+    .append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+
+
+    svg
+    .append('path')
+    .attr('d', function(d) {
+      return lineus(d.values)
+    })
+    .attr('stroke', '#2b8cbe')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none')
+    .lower()
+    // .each(function(d) {
+    //   var g = d3.select(this)
+
+    //   var datapoints = d.values
+    //   console.log(datapoints)}
+
+      // Need to use + here to convert to numbers
+      // let maxHigh = d3.max(datapoints, d => +d.high)
+      // let minHigh = d3.min(datapoints, d => +d.high)
+
+      // g.append('circle')
+      //   .attr('r', 7)
+      //   .attr('fill', 'pink')
+      //   .attr('cy', 0)
+      //   .attr('cx', xPositionScale(maxHigh))
+
+      // g.append('circle')
+      //   .attr('r', 7)
+      //   .attr('fill', 'lightblue')
+      //   .attr('cy', 0)
+      //   .attr('cx', xPositionScale(minHigh))
+
+      // g.append('line')
+      //   .attr('y1', 0)
+      //   .attr('y2', 0)
+      //   .attr('x1', xPositionScale(minHigh))
+      //   .attr('x2', xPositionScale(maxHigh))
+      //   .attr('stroke', 'grey')
+      //   .lower()
+
+      // Add your axes
+  var xAxis = d3
+    .axisBottom(xPositionScale)
+    .tickFormat(d3.timeFormat('%b %y'))
+    .ticks(10)
+  svg
+    .append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+
+  var yAxis = d3
+    .axisLeft(yPositionScale)
+    .tickValues([0.0, 0.1, 0.2, 0.3])
+
+  svg
+    .append('g')
+    .attr('class', 'axis y-axis')
+    .call(yAxis)
+
+}
