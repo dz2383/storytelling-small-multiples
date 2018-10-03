31cdb74f974212a58f4c08b3abb5738eced4b52e
Dian <dz2382@columbia.edu>
2018-10-03 02:19:20 -0400

    chart3

--- src/chart-2.js
+++ src/chart-2.js
@@ -1,9 +1,9 @@
 import * as d3 from 'd3'
 
 // Set up margin/height/width
-var margin = { top: 30, left: 20, right: 20, bottom: 20 }
+var margin = { top: 30, left: 30, right: 10, bottom: 20 }
 
-var height = 150 - margin.top - margin.bottom
+var height = 130 - margin.top - margin.bottom
 var width = 100 - margin.left - margin.right
 
 // I'll give you the container
@@ -16,13 +16,8 @@ var yPositionScale = d3
   .domain([0, 0.3])
   .range([height, 0])
 
-// var colorScale = d3
-//   .scaleOrdinal()
-//   .domain(['ASFR_jp', 'ASFR_us'])
-//   .range(['#e34a33', '#2b8cbe'])
-
 // Create a d3.line function that uses your scales
-var lineus = d3
+var line_us = d3
   .line()
   .x(function(d) {
     return xPositionScale(d.Age)
@@ -30,9 +25,9 @@ var lineus = d3
   .y(function(d) {
     return yPositionScale(d.ASFR_us)
   })
-  .curve(d3.curveMonotoneX)
+  //.curve(d3.curveMonotoneX)
 
-var linejp = d3
+var line_jp = d3
   .line()
   .x(function(d) {
     return xPositionScale(d.Age)
@@ -40,7 +35,10 @@ var linejp = d3
   .y(function(d) {
     return yPositionScale(d.ASFR_jp)
   })
-  .curve(d3.curveMonotoneX)
+  //.curve(d3.curveMonotoneX)
+
+// area = d3.area()
+
 
 // Read in your data
 d3.csv(require('./fertility.csv'))
@@ -56,73 +54,96 @@ function ready(datapoints) {
     .nest()
     .key(d => d.Year)
     .entries(datapoints)
-  console.log(nested)
+  // console.log(nested)
+
+  // pull out ages on x axis
+  var ages = datapoints.map(d => d.Age)
+  xPositionScale.domain(d3.extent(ages))
 
   // Draw your lines
   container
-    .selectAll('svg')
+    .selectAll('.fertility-svg')
     .data(nested)
     .enter()
     .append('svg')
+    .attr('class','fertility-svg')
     .attr('height', height + margin.top + margin.bottom)
     .attr('width', width + margin.left + margin.right)
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`)
+    .each(function(d) {
+      var svg = d3.select(this)
 
-  svg
-    .append('path')
-    .attr('d', function(d) {
-      return lineus(d.values)
+      var datapoints = d.values
+      console.log(datapoints)
+
+      let rateUS = d3.sum(datapoints, d => +d.ASFR_us).toFixed(2)
+      let rateJP = d3.sum(datapoints, d => +d.ASFR_jp).toFixed(2)
+
+      // Add your pathes
+      svg
+        .append('path')
+        // why I cannot use .data(datapoints)
+        .datum(datapoints)
+        .attr('d', line_us)
+        .attr('stroke', 'lightblue')
+        .attr('fill', 'lightblue')
+        .attr('opacity', 0.6)
+
+      svg
+        .append('path')
+        .datum(datapoints)
+        .attr('d', line_jp)
+        .attr('stroke', 'red')
+        .attr('fill', 'red')
+        .attr('opacity', 0.4)
+        .lower()
+
+      // Add your notations
+      svg
+        .append('text')
+        .attr('x', (width * 3) / 4)
+        .attr('y', height / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', 8)
+        .style('fill', 'lightblue')
+        .text(rateUS)
+
+      svg
+        .append('text')
+        .attr('x', (width * 3) / 4)
+        .attr('y', height / 2 + 10)
+        .attr('text-anchor', 'middle')
+        .style('font-size', 8)
+        .style('fill', 'red')
+        .text(rateJP)
+
+      // Add your title
+      svg
+        .append('text')
+        .attr('x', width / 2)
+        .attr('y', 0 - margin.top / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', 10)
+        .text(d.key)
+
+      // Add your axes
+      var xAxis = d3.axisBottom(xPositionScale).tickValues([15, 30, 45])
+
+      svg
+        .append('g')
+        .attr('class', 'axis x-axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis)
+
+      var yAxis = d3
+        .axisLeft(yPositionScale)
+        .tickValues([0.0, 0.1, 0.2, 0.3])
+        .ticks(4)
+
+      svg
+        .append('g')
+        .attr('class', 'axis y-axis')
+        .call(yAxis)
     })
-    .attr('stroke', '#2b8cbe')
-    .attr('stroke-width', 2)
-    .attr('fill', 'none')
-    .lower()
-  // .each(function(d) {
-  //   var g = d3.select(this)
-
-  //   var datapoints = d.values
-  //   console.log(datapoints)}
-
-  // Need to use + here to convert to numbers
-  // let maxHigh = d3.max(datapoints, d => +d.high)
-  // let minHigh = d3.min(datapoints, d => +d.high)
-
-  // g.append('circle')
-  //   .attr('r', 7)
-  //   .attr('fill', 'pink')
-  //   .attr('cy', 0)
-  //   .attr('cx', xPositionScale(maxHigh))
-
-  // g.append('circle')
-  //   .attr('r', 7)
-  //   .attr('fill', 'lightblue')
-  //   .attr('cy', 0)
-  //   .attr('cx', xPositionScale(minHigh))
-
-  // g.append('line')
-  //   .attr('y1', 0)
-  //   .attr('y2', 0)
-  //   .attr('x1', xPositionScale(minHigh))
-  //   .attr('x2', xPositionScale(maxHigh))
-  //   .attr('stroke', 'grey')
-  //   .lower()
-
-  // Add your axes
-  var xAxis = d3
-    .axisBottom(xPositionScale)
-    .tickFormat(d3.timeFormat('%b %y'))
-    .ticks(10)
-  svg
-    .append('g')
-    .attr('class', 'axis x-axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis)
-
-  var yAxis = d3.axisLeft(yPositionScale).tickValues([0.0, 0.1, 0.2, 0.3])
-
-  svg
-    .append('g')
-    .attr('class', 'axis y-axis')
-    .call(yAxis)
 }
--- src/chart-3.js
+++ src/chart-3.js
@@ -1,15 +1,131 @@
 import * as d3 from 'd3'
 
 // Create your margins and height/width
+var margin = { top: 30, left: 40, right: 10, bottom: 20 }
+
+var height = 200 - margin.top - margin.bottom
+var width = 150 - margin.left - margin.right
 
 // I'll give you this part!
 var container = d3.select('#chart-3')
 
 // Create your scales
+var xPositionScale = d3.scaleLinear().range([0, width])
+var yPositionScale = d3
+  .scaleLinear()
+  .domain([0, 20000])
+  .range([height, 0])
 
 // Create your line generator
+var line = d3
+  .line()
+  .x(function(d) {
+    return xPositionScale(d.year)
+  })
+  .y(function(d) {
+    return yPositionScale(d.income)
+  })
 
 // Read in your data
+Promise.all([
+  d3.csv(require('./middle-class-income.csv')),
+  d3.csv(require('./middle-class-income-usa.csv'))
+]).then(ready)
 
-// Create your ready function
+// Build your ready function that draws lines, axes, etc
+function ready([datapoints_world, datapoints_us]) {
+  // Group your data together
+  var nested = d3
+    .nest()
+    .key(d => d.country)
+    .entries(datapoints_world)
+  console.log(nested)
 
+  // put out years on x axis
+  // use  d3.extent() !
+  var incomeYear = datapoints_world.map(d => d.year)
+  xPositionScale.domain(d3.extent(incomeYear))
+
+  container
+    .selectAll('svg')
+    .data(nested)
+    .enter()
+    .append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+    .each(function(d) {
+      var svg = d3.select(this)
+
+      var datapoints = d.values
+      console.log(datapoints)
+
+      svg
+        .append('path')
+        .datum(datapoints)
+        .attr('d', line)
+        .attr('stroke', '#c51b8a')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none')
+
+      svg
+        .append('path')
+        .datum(datapoints_us)
+        .attr('d', line)
+        .attr('stroke', 'gray')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none')
+
+      // Add your title
+      svg
+        .append('text')
+        .attr('x', width / 2)
+        .attr('y', 0 - margin.top / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', 14)
+        .style('fill', '#c51b8a')
+        .attr('font-weight', 'bold')
+        .text(d.key)
+      // Add USA notation
+      svg
+        .append('text')
+        .text('USA')
+        .attr('x', 10)
+        .attr('y', 20)
+        .attr('font-size', 10)
+        .attr('fill', 'gray')
+
+      // Add your axes
+      var xAxis = d3
+        .axisBottom(xPositionScale)
+        .tickValues([1980, 1990, 2000, 2010])
+        .tickSize(-height)
+
+      svg
+        .append('g')
+        .attr('class', 'axis x-axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .style('stroke-dasharray', '2, 2')
+        .call(xAxis)
+
+      var yAxis = d3
+        .axisLeft(yPositionScale)
+        .tickFormat(d3.format('$,d'))
+        .tickSize(-width)
+        // .tickValues([50000, 10000, 15000, 20000])
+        .ticks(4)
+
+      svg
+        .append('g')
+        .attr('class', 'axis y-axis')
+        .style('stroke-dasharray', '2, 2')
+        .call(yAxis)
+
+      // Move our axes below everything else
+      svg.select('.axis').lower()
+
+      // Remove the weird lines
+      svg.selectAll('.domain').remove()
+    })
+}
